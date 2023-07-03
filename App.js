f520b7d96c4729bc70a368d2cc3e68e272dedd1c
sanketudaypatil <sanketpatil@nimapinfotech.com>
2023-07-03 14:52:21 +0530

    strart create header element

--- App.js
+++ App.js
@@ -1,38 +1,17 @@
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import React, {useState} from 'react';
-import HomeBanner from './components/Homebanner';
-import Counter from './components/Counter';
-import HighOrderComponent from './components/HighOrderComponent';
-import RedHOC from './components/RedHOC';
-import BlueHOC from './components/BlueHOC';
-import PinkHOC from './components/PinkHOC';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import AppHeader from './component/AppHeader'
 
-// import Products from './components/Products'
 
 const App = () => {
   return (
     <View>
-      <View style={styles.bgContainer}>
-        <Text style={styles.text}>High Order Components</Text>
-        <HighOrderComponent cmp={Counter} />
-        <RedHOC cmp={Counter} />
-        <BlueHOC cmp={Counter} />
-        <PinkHOC cmp={Counter} />
-      </View>
+      {/* <AppHeader/> */}
+      <Text>sanket</Text>
     </View>
-  );
-};
+  )
+}
 
-export default App;
+export default App
 
-const styles = StyleSheet.create({
-  bgContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    top: 45,
-  },
-  text: {
-    color: 'blue',
-    fontSize: 22,
-  },
-});
+const styles = StyleSheet.create({})
